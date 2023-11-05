8a019e2564077b3ce833167551fae1a13333b4c8
dfhernandezp12 <142235794+dfhernandezp12@users.noreply.github.com>
2023-11-05 11:06:56 -0500

    se añaden los test del listado

--- src/app/vehiculo/vehiculo-list/vehiculo-list.component.spec.ts
+++ src/app/vehiculo/vehiculo-list/vehiculo-list.component.spec.ts
@@ -6,15 +6,19 @@ import { faker } from '@faker-js/faker';
 
 import { VehiculoListComponent } from './vehiculo-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { VehiculoService } from '../vehiculo.service';
+import { Vehiculo } from '../vehiculo';
 
 describe('VehiculoListComponent', () => {
   let component: VehiculoListComponent;
   let fixture: ComponentFixture<VehiculoListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports:[HttpClientModule],
-      declarations: [ VehiculoListComponent ]
+      declarations: [ VehiculoListComponent ],
+      providers: [VehiculoService]
     })
     .compileComponents();
   }));
@@ -22,10 +26,53 @@ describe('VehiculoListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(VehiculoListComponent);
     component = fixture.componentInstance;
+
+    for(let i = 0; i < 10; i++) {
+      const vehiculo = new Vehiculo(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence()
+      );
+      component.vehiculos.push(vehiculo);
+      }
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have 30 <td> elements', () => {
+    expect(debug.queryAll(By.css('td'))).toHaveSize(30)
+  });
+
+  it('should have 14 <td> elements', () => {
+    expect(debug.queryAll(By.css('th'))).toHaveSize(14)
+  });
+
+  it('should have 1 <img> elements', () => {
+    expect(debug.queryAll(By.css('img'))).toHaveSize(1)
+  });
+
+  it('should have 1 <h1> elements', () => {
+    expect(debug.queryAll(By.css('h1'))).toHaveSize(1)
+  });
+
+  it('should have 1 <h1> elements', () => {
+    const conteoMarcas: string | string[] = [];
+    component.vehiculos.forEach((vehiculo) => {
+       if (!conteoMarcas.includes(vehiculo.marca)) {
+        conteoMarcas.push(vehiculo.marca)
+       }
+    });
+    expect(debug.queryAll(By.css('p'))).toHaveSize(conteoMarcas.length)
+  });
+
 });
