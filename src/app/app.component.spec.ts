aab239e9e84deee53cfe24c81cfefb3d2af3cefe
dfhernandezp12 <142235794+dfhernandezp12@users.noreply.github.com>
2023-11-05 10:27:01 -0500

    ajuste de pruebas iniciales

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { VehiculoListComponent } from './vehiculo/vehiculo-list/vehiculo-list.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    imports: [RouterTestingModule,HttpClientModule],
+    declarations: [AppComponent, VehiculoListComponent]
   }));
 
   it('should create the app', () => {
@@ -20,10 +22,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('EvaluacionAngular');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('EvaluacionAngular app is running!');
-  });
 });
